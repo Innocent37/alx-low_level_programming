5a31851550acbe9f1cae1f9c519236a5a116fa22
Innocent <ndumisomaseko37@gmail.com>
2023-02-20 08:47:07 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 			{
 				if (l > m && m > n)
 				{
-		putchar(n);
+putchar(n);
 					putchar(m);
 					putchar(l);
 					if (n != 55 || m != 56)
@@ -31,5 +31,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-
