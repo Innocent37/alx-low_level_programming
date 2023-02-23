6eda6d5b2e1fe6c85cc93780d31d479edc9e1f60
Innocent <ndumisomaseko37@gmail.com>
2023-02-23 11:09:59 -0800

    ahhh

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *positive_or_nagative - checks if the number nagative or positive
+ *
+ *@i: is the number checked
+ *Return: always 0
+ */
+void positive_or_negative(int i);
+{
+	if (i < 0)
+		printf("%d is nagativ\ne", i);
+	else
+		printf("%d is positive\n", i);
+}
