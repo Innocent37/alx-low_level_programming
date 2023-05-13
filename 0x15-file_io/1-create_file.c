93bce1a3b483db6591015a54e8a35b49ae521f7a
Innocent37 <ndumisomaseko37@gmail.com>
2023-05-13 06:44:28 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,28 +14,37 @@
  * Return: 1 is fails else -1
  */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
+	int len = 0;
+	FILE *file = NULL;
+	int w = 0;
+
 	if (filename == NULL)
-		return (-1);
-
-	int file_d;
-		ssize_t result;
-
-	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_d == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		result = write(file_d, text_content, strlen(text_content));
-		if (result == -1)
-		{
-			close(file_d);
-			return (-1);
-		}
+
+		while (text_content[len] != '\0')
+			len++;
+	}
+		file = fopen(filename, "w");
+		if (file == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+
+		w = fwrite(text_content, sizeof(char), len, file);
+	if (w != len)
+	{
+
+		fclose(file);
+		return (-1);
+	}
 	}
 
-	close(file_d);
-		return (1);
+	fclose(file);
+	return (-1);
 }
