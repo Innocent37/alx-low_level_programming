d46430d528aa777920bd5fa67e62066c473f646d
Innocent37 <ndumisomaseko37@gmail.com>
2023-07-06 05:33:09 -0700

    binary2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,45 +1,26 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdio.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: Pointer to the string representing the binary number
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: The converted unsigned int, or 0 on error
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int results = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	for (; *b; b++)
+	for (i = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		results = (results << 1) + (*b - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (results);
-}
-
-int main(void)
-{
-	const char *binary1 = "1";
-	const char *binary2 = "101";
-	const char *binary3 = "0";
-	const char *binary4 = "1100010";
-	const char *binary5 = "110010010";
-
-	printf("%u\n", binary_to_uint(binary1));
-	printf("%u\n", binary_to_uint(binary2));
-	printf("%u\n", binary_to_uint(binary3));
-	printf("%u\n", binary_to_uint(binary4));
-	printf("%u\n", binary_to_uint(binary5));
-
-	return (0);
+	return (dec_val);
 }
 
