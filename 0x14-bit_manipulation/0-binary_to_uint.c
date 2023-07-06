9ffb3904867fc32641cfd68916db0d8230fc7ced
Innocent37 <ndumisomaseko37@gmail.com>
2023-07-06 05:26:02 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int
@@ -8,7 +10,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int results = 0;
 
 	if (b == NULL)
 		return (0);
@@ -18,9 +20,26 @@ unsigned int binary_to_uint(const char *b)
 		if (*b != '0' && *b != '1')
 			return (0);
 
-		result = (result << 1) + (*b - '0');
+		results = (results << 1) + (*b - '0');
 	}
 
-	return (result);
+	return (results);
+}
+
+int main(void)
+{
+	const char *binary1 = "1";
+	const char *binary2 = "101";
+	const char *binary3 = "0";
+	const char *binary4 = "1100010";
+	const char *binary5 = "110010010";
+
+	printf("%u\n", binary_to_uint(binary1));
+	printf("%u\n", binary_to_uint(binary2));
+	printf("%u\n", binary_to_uint(binary3));
+	printf("%u\n", binary_to_uint(binary4));
+	printf("%u\n", binary_to_uint(binary5));
+
+	return (0);
 }
 
