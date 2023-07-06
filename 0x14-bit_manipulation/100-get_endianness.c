9e4f19aea6aede3c464f395e158105f61d5a582a
Innocent37 <ndumisomaseko37@gmail.com>
2023-07-06 04:56:45 -0700

    bitwise operators

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * get_endianness - checks the endianess of MACHINE
- *
- * Return: 0 if true then 1 if flase
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big endian , 1 for little
  */
+
 int get_endianness(void)
 {
-	unsigned int c = 1;
-	char *x = (char *)&x;
+	int i = 1;
+	char *c = (char *)&i;
 
-	return (*x ? 1 : 0);
+	return ((int)*c);
 }
+
