423807987ab036f1899ade4d1b73573bed0bbb70
Innocent37 <ndumisomaseko37@gmail.com>
2023-07-10 13:33:29 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,35 +9,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	/* Variable Declarations */
 	FILE *file;
 	int fd, w, len = 0;
 
-	/* Check if filename is NULL */
 	if (filename == NULL)
 		return (-1);
 
-	/* Calculate length of text_content */
 	if (text_content != NULL)
 	{
 		while (text_content[len])
 			len++;
 	}
-
-	/* Open the file using open() */
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	/* Associate a FILE* stream with the file descriptor */
 	file = fdopen(fd, "w");
 	if (file == NULL)
 	{
 		close(fd);
 		return (-1);
 	}
-
-	/* Write text_content to the file */
 	if (text_content != NULL)
 	{
 		w = fwrite(text_content, sizeof(char), len, file);
@@ -47,9 +39,6 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-
-	/* Close the file and return success */
 	fclose(file);
 	return (1);
 }
-
